82bfffd49226dc55753cf9bee3c315fa5cbdfc89
AlbaMartin01 <albmar891@alu.centroafuera.es>
2022-12-15 14:06:45 +0100

    Intento Listeners A

--- app/src/main/java/com/example/ejercicio4listeners/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio4listeners/MainActivity.kt
@@ -1,70 +1,44 @@
 package com.example.ejercicio4listeners
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.Toast
 import com.example.ejercicio4listeners.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    private var onCreate = 0
-    private var onStart = 1
-    private var onResume = 2
-    private var onPause = 3
-    private var onRestart = 4
-    private var onStop = 5
-    private var onDestroy = 6
-    private var onSave = 7
-    private var onRestore = 8
-
-    etContent.addTextChangedListener(object : TextWatcher{
-        override fun afterTextChanged(s: Editable?) {}
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int){}
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-    })
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("onStart $onCreate")
+        //addListenerToEditText()
+
+        val editText = findViewById<EditText>(R.id.editText)
+        editText.addTextChangedListener(object : TextWatcher{
+                override fun afterTextChanged(p0: Editable?) {
+                    binding.textView.text = binding.editText.text
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
     }
 
-    override fun onStart() {
-        super.onStart()
-        setContentView(R.layout.activity_main)
-        println("onStart $onStart")
-    }
-    override fun onResume() {
-        super.onResume()
-        setContentView(R.layout.activity_main)
-        println("onResume $onResume")
-    }
-    override fun onPause() {
-        super.onPause()
-        setContentView(R.layout.activity_main)
-        println("onPause $onPause")
-    }
-    override fun onRestart() {
-        super.onRestart()
-        setContentView(R.layout.activity_main)
-        println("onRestart $onRestart")
-    }
-    override fun onStop() {
-        super.onStop()
-        setContentView(R.layout.activity_main)
-        println("onStop $onStop")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        setContentView(R.layout.activity_main)
-        println("onDestroy $onDestroy")
-    }
-    private fun addListenerToEditText(){
-        binding.etContent
-    }
+    /*private fun addListenerToEditText(){
+        binding.editText.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                binding.textView.text = binding.editText.text
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+    }*/
+
 }
